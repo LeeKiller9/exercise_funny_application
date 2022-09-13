750ab7fc53d99e505d13b4d654aea13fa788c87d
LeeKiller9 <112848883+LeeKiller9@users.noreply.github.com>
2022-09-13 23:12:29 +0700

    update js file

--- javascript.js
+++ javascript.js
@@ -5,12 +5,13 @@ function yesAnswer() {
 }
 
 function noAnswer() {
-    let posX = Math.floor(Math.random() * 1501 - 200)
-    let posY = Math.floor(Math.random() * 651 - 150)
+    let width = window.innerWidth
+    let height = window.innerHeight
+    let posX = Math.floor(Math.random() * width) - 200
+    let posY = Math.floor(Math.random() * height) - 120
     noBtn.style.left = posX + 'px' //-200 -> 1300
     noBtn.style.top = posY + 'px' //-150 ->  500
 }
-
 
 function init() {
     noBtn.style.position = 'relative'
